18c44cde02887dcf6c237331c134f16a7fe567e4
Romainub87 <romaincordier33160@gmail.com>
2025-01-23 20:25:58 +0100

    feat(design): intégration répartition victoires avec MVP

--- script/average.js
+++ script/average.js
@@ -1,172 +1,119 @@
-d3.json('../data/data.json').then(data => {
 
-    const stats = {
-        'FG%': 'Pourcentage de tir réussis',
-        'PPG': 'Points par match',
-        'RPG': 'Rebonds par match',
-        'APG': 'Passes décisives par match',
-        'BLKPG': 'Contres par match',
-    };
+export function updateAverageChart(startYearSelected, endYearSelected, statKey, data, stats) {
+    const validData = data.filter(player => player.year >= startYearSelected && player.year <= endYearSelected && player[statKey] != null);
+    const average = statKey.includes('%') ? (validData.reduce((acc, player) => acc + player[statKey], 0) / validData.length) * 100 : validData.reduce((acc, player) => acc + player[statKey], 0) / validData.length;
 
-    let startYear = Math.min(...data.filter(player => Object.keys(stats).some(statKey => player[statKey] != null)).map(player => player.year));
-    let endYear = Math.max(...data.filter(player => Object.keys(stats).some(statKey => player[statKey] != null)).map(player => player.year));
+    const svg = d3.select(statKey.includes('%') ? '#chart' : '#subcharts').append('svg')
+        .attr('width', statKey.includes('%') ? 400 : 300)
+        .attr('height', statKey.includes('%') ? 400 : 100)
+        .attr('viewBox', statKey.includes('%') ? '0 0 400 400' : '0 0 200 200')
+        .attr('preserveAspectRatio', 'xMidYMid meet');
 
-    for (const statKey in stats) {
-        update(startYear, endYear, statKey);
-    }
+    const width = statKey.includes('%') ? 400 : 200;
+    const height = width / 2;
+    const outerRadius = height / 1.5;
+    const innerRadius = outerRadius * 0.75;
 
-    function update(startYearSelected, endYearSelected, statKey) {
+    svg.append('defs').append('linearGradient')
+        .attr('id', 'gradient')
+        .attr('x1', '0%')
+        .attr('y1', '0%')
+        .attr('x2', '0%')
+        .attr('y2', '100%')
+        .selectAll('stop')
+        .data([
+            { offset: '0%', color: '#00f' },
+            { offset: '100%', color: '#f00' }
+        ])
+        .enter().append('stop')
+        .attr('offset', d => d.offset)
+        .attr('stop-color', d => d.color);
 
-        const validData = data.filter(player => player.year >= startYearSelected && player.year <= endYearSelected && player[statKey] != null);
-        const average = statKey.includes('%') ? (validData.reduce((acc, player) => acc + player[statKey], 0) / validData.length) * 100 : validData.reduce((acc, player) => acc + player[statKey], 0) / validData.length;
+    if (!statKey.includes('%')) {
+        svg.append('text')
+            .attr('transform', 'translate(100, 50)')
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '50px')
+            .style('opacity', 0)
+            .text(`${average.toFixed(2)}`)
+            .transition()
+            .duration(1000)
+            .style('opacity', 1);
 
-        const svg = d3.select(statKey.includes('%') ? '#chart' : '#subcharts').append('svg')
-            .attr('width', statKey.includes('%') ? 400 : 250)
-            .attr('height', statKey.includes('%') ? 400 : 100) // Changed height to 200 for consistency
-            .attr('viewBox', statKey.includes('%') ? '0 0 400 400' : '0 0 200 200')
-            .attr('preserveAspectRatio', 'xMidYMid meet');
+        svg.append('text')
+            .attr('transform', 'translate(100, 50)')
+            .attr('dy', '1.5em')
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '40px')
+            .style('opacity', 0)
+            .text(stats[statKey])
+            .transition()
+            .duration(1000)
+            .style('opacity', 1);
+    } else {
+        const arcGenerator = d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius);
 
-        const width = statKey.includes('%') ? 400 : 200; // Adjusted width based on statKey
-        const height = width / 2; // Simplified height calculation
-        const outerRadius = height / 1.5; // Adjusted outerRadius calculation
-        const innerRadius = outerRadius * 0.75;
+        const pie = d3.pie()
+            .value(100);
 
+        const arcs = svg.selectAll('.arc')
+            .data(pie([average, 100 - average]))
+            .enter().append('g')
+            .attr('class', 'arc')
+            .attr('transform', 'translate(200, 200)');
 
-        svg.append('defs').append('linearGradient')
-            .attr('id', 'gradient')
-            .attr('x1', '0%')
-            .attr('y1', '0%')
-            .attr('x2', '0%')
-            .attr('y2', '100%')
-            .selectAll('stop')
-            .data([
-                { offset: '0%', color: '#00f' },
-                { offset: '100%', color: '#f00' }
-            ])
-            .enter().append('stop')
-            .attr('offset', d => d.offset)
-            .attr('stop-color', d => d.color);
-
-        if (!statKey.includes('%')) {
-            svg.append('text')
-                .attr('transform', 'translate(100, 50)')
-                .attr('text-anchor', 'middle')
-                .attr('font-size', '50px')
-                .style('opacity', 0)
-                .text(`${average.toFixed(2)}`)
-                .transition()
-                .duration(1000)
-                .style('opacity', 1);
-
-            svg.append('text')
-                .attr('transform', 'translate(100, 50)')
-                .attr('dy', '1.5em')
-                .attr('text-anchor', 'middle')
-                .attr('font-size', '40px')
-                .style('opacity', 0)
-                .text(stats[statKey])
-                .transition()
-                .duration(1000)
-                .style('opacity', 1);
-        } else {
-            const arcGenerator = d3.arc()
-                .innerRadius(innerRadius)
-                .outerRadius(outerRadius);
-
-            const pie = d3.pie()
-                .value(d => 100);
-
-            const arcs = svg.selectAll('.arc')
-                .data(pie([average, 100 - average]))
-                .enter().append('g')
-                .attr('class', 'arc')
-                .attr('transform', 'translate(200, 200)');
-
-            // Background arc
-            arcs.append('path')
-                .attr('d', arcGenerator)
-                .attr('fill', '#d3d3d3')
-                .transition()
-                .duration(1000)
-                .attrTween('d', function (d) {
-                    const interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, {
-                        startAngle: 0,
-                        endAngle: (100 / 100) * 2 * Math.PI
-                    });
-                    return function (t) {
-                        return arcGenerator(interpolate(t));
-                    };
+        arcs.append('path')
+            .attr('d', arcGenerator)
+            .attr('fill', '#d3d3d3')
+            .transition()
+            .duration(1000)
+            .attrTween('d', function () {
+                const interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, {
+                    startAngle: 0,
+                    endAngle: (100 / 100) * 2 * Math.PI
                 });
-
-            // Foreground arc
-            arcs.append('path')
-                .attr('d', arcGenerator.cornerRadius(10))
-                .attr('fill', 'url(#gradient)')
-                .transition()
-                .duration(1500)
-                .attrTween('d', function (d) {
-                    const interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, {
-                        startAngle: 0,
-                        endAngle: (average / 100) * 2 * Math.PI
-                    });
-                    return function (t) {
-                        return arcGenerator(interpolate(t));
-                    };
-                });
-
-            svg.append('text')
-                .attr('transform', 'translate(200, 30)') // Position above the center
-                .attr('dy', '0.35em')
-                .attr('text-anchor', 'middle')
-                .attr('font-size', '30px')
-                .style('opacity', 0)
-                .text(stats[statKey])
-                .transition()
-                .duration(1000)
-                .style('opacity', 1);
-
-            svg.append('text')
-                .attr('transform', 'translate(200, 200)')
-                .attr('dy', '0.35em')
-                .attr('text-anchor', 'middle')
-                .attr('font-size', '40px')
-                .style('opacity', 0)
-                .text(`${average.toFixed(2)}%`)
-                .transition()
-                .duration(1000)
-                .style('opacity', 1);
-        }
-
-        $( function() {
-            let delayTimeout;
-            $( "#intervalleAnnee" ).slider({
-                range: true,
-                min: 1987,
-                max: 2024,
-                values: [ startYearSelected, endYearSelected ],
-                slide: function( event, ui ) {
-                    clearTimeout(delayTimeout);
-                    console.log(ui.values);
-                    delayTimeout = setTimeout(function() {
-                        startYear = ui.values[ 0 ];
-                        endYear = ui.values[ 1 ];
-                        d3.select('#subcharts').selectAll('svg').transition().duration(500).style('opacity', 0).remove();
-                        d3.select('#chart').selectAll('svg').transition().duration(500).style('opacity', 0).remove().on('end', () => {
-                            for (const statKey in stats) {
-                                update(startYear, endYear, statKey);
-                            }
-                        });
-                        $( "#anneeLabel" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
-                    }, 500);
-                }
-            }).css({
-                'border': '1px solid #999',
-                'border-radius': '10px',
-                'box-shadow': '0 4px 8px rgba(0, 0, 0, 0.2)'
+                return function (t) {
+                    return arcGenerator(interpolate(t));
+                };
             });
 
-            $( "#anneeLabel" ).val($( "#intervalleAnnee" ).slider("values", 0) + " - " + $( "#intervalleAnnee" ).slider("values", 1));
-        });
-    }
+        arcs.append('path')
+            .attr('d', arcGenerator.cornerRadius(10))
+            .attr('fill', 'url(#gradient)')
+            .transition()
+            .duration(1500)
+            .attrTween('d', function () {
+                const interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, {
+                    startAngle: 0,
+                    endAngle: (average / 100) * 2 * Math.PI
+                });
+                return function (t) {
+                    return arcGenerator(interpolate(t));
+                };
+            });
 
-});
+        svg.append('text')
+            .attr('transform', 'translate(200, 30)')
+            .attr('dy', '0.35em')
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '30px')
+            .style('opacity', 0)
+            .text(stats[statKey])
+            .transition()
+            .duration(1000)
+            .style('opacity', 1);
+
+        svg.append('text')
+            .attr('transform', 'translate(200, 200)')
+            .attr('dy', '0.35em')
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '40px')
+            .style('opacity', 0)
+            .text(`${average.toFixed(2)}%`)
+            .transition()
+            .duration(1000)
+            .style('opacity', 1);
+    }
+}
--- script/camembert.js
+++ script/camembert.js
@@ -1,87 +1,143 @@
-// Load JSON data from the data directory
 
-d3.json('data/data.json').then(data => {
+export function createCamembert(startYearSelected, endYearSelected, data) {
+    const mvpData = data.filter(d => d.year >= startYearSelected && d.year <= endYearSelected && d.player !== null);
+    const totalTeams = mvpData.length;
+    const championshipWins = mvpData.filter(d => d.teamWonChampionship).length;
+    const noChampionshipWins = totalTeams - championshipWins;
 
-    let startYear = Math.min(...data.map(d => d.year));
-    let endYear = Math.max(...data.map(d => d.year));
+    console.log('startYearSelected', startYearSelected);
+    console.log('endYearSelected', endYearSelected);
+    console.log('totalTeams', totalTeams);
+    console.log('championshipWins', championshipWins);
+    console.log('noChampionshipWins', noChampionshipWins);
 
-    createCamembert(startYear, endYear);
+    const dataset = [
+        { label: "Victoire avec MVP", value: championshipWins },
+        { label: "Défaite avec MVP", value: noChampionshipWins },
+    ];
 
-    function createCamembert(startYearSelected = startYear, endYearSelected = endYear) {
-        console.log(data);
-        const mvpData = data.filter(d => d.year >= startYear && d.year <= endYear && d.player !== null);
-        const totalTeams = mvpData.length;
-        const championshipWins = mvpData.filter(d => d.teamWonChampionship).length;
-        const noChampionshipWins = totalTeams - championshipWins;
+    const width = 600;
+    const height = 600;
+    const radius = Math.min(width, height) / 2;
+    const innerRadius = radius * 0.02;
 
-        const dataset = [
-            { label: "Victoire avec MVP", value: championshipWins },
-            { label: "Défaite avec MVP", value: noChampionshipWins },
-        ];
+    const color = d3.scaleOrdinal()
+        .domain(dataset.map(d => d.label))
+        .range(["#5cb7e9", "#fc0a2c"]);
 
-        const width = 350;
-        const height = 350;
-        const radius = Math.min(width, height) / 2;
+    let svg = d3.select('#camembert').select('svg');
 
-        const color = d3.scaleOrdinal()
-            .domain(dataset.map(d => d.label))
-            .range(["#5cb7e9", "#fc0a2c"]);
-
-        const svg = d3.select('#camembert')
+    if (svg.empty()) {
+        svg = d3.select('#camembert')
             .append('svg')
             .attr('width', width)
             .attr('height', height)
+            .attr('viewBox', `0 0 ${width} ${height}`)
             .append('g')
-            .attr('transform', `translate(${width / 2}, ${height / 2})`);
-
-        const pie = d3.pie()
-            .value(d => d.value);
-
-        const data_ready = pie(dataset);
-
-        const arc = d3.arc()
-            .innerRadius(0)
-            .outerRadius(radius);
-
-        svg.selectAll('path')
-            .data(data_ready)
-            .join('path')
-            .attr('d', arc)
-            .attr('fill', d => color(d.data.label));
-
-        svg.selectAll('text')
-            .data(data_ready)
-            .join('text')
-            .text(d => `${d.data.label}: ${((d.data.value / totalTeams) * 100).toFixed(1)}%`)
-            .attr('transform', d => `translate(${arc.centroid(d)})`)
-            .style('text-anchor', 'middle')
-            .style('font-size', '14px');
-
-        $( function() {
-            let delayTimeout;
-            $( "#intervalleAnnee" ).slider({
-                range: true,
-                min: 1987,
-                max: 2024,
-                values: [ startYearSelected, endYearSelected ],
-                slide: function( event, ui ) {
-                    clearTimeout(delayTimeout);
-                    delayTimeout = setTimeout(function() {
-                        startYear = ui.values[ 0 ];
-                        endYear = ui.values[ 1 ];
-                        $( "#anneeLabel" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
-                        createCamembert(startYear, endYear);
-                    }, 500); // Délai en millisecondes
-                }
-            }).css({
-                'border': '1px solid #999',
-                'border-radius': '10px',
-                'box-shadow': '0 4px 8px rgba(0, 0, 0, 0.2)'
-            });
-
-            $( "#anneeLabel" ).val($( "#intervalleAnnee" ).slider("values", 0) + " - " + $( "#intervalleAnnee" ).slider("values", 1));
-        });
+            .attr('transform', `translate(${width / 2}, ${height / 2}) scale(0.8)`);
+    } else {
+        svg.select('g')
+            .attr('transform', `translate(${width / 2}, ${height / 2}) scale(0.8)`);
     }
-}).catch(error => {
-    console.error('Error loading the JSON data:', error);
-});
+
+    const pie = d3.pie()
+        .value(d => d.value)
+        .padAngle(0.05);
+
+    const data_ready = pie(dataset);
+
+    const arc = d3.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(radius)
+        .cornerRadius(5);
+
+    const paths = svg.selectAll('path')
+        .data(data_ready);
+
+    paths.enter().append('path')
+        .attr('d', arc)
+        .attr('fill', d => color(d.data.label))
+        .attr('stroke', '#fff')
+        .attr('stroke-width', '2px')
+        .style('filter', 'url(#drop-shadow)')
+        .merge(paths)
+        .transition()
+        .duration(1000)
+        .attrTween('d', function (d) {
+            const interpolate = d3.interpolate(this._current || {startAngle: 0, endAngle: 0}, d);
+            this._current = interpolate(1);
+            return function (t) {
+                return arc(interpolate(t));
+            };
+        });
+
+    paths.exit().remove();
+
+    const texts = svg.selectAll('text.label')
+        .data(data_ready);
+
+    texts.enter().append('text')
+        .attr('class', 'label')
+        .text(d => `${d.data.label}`)
+        .attr('transform', d => `translate(${arc.centroid(d)[0]}, ${arc.centroid(d)[1] - 20})`)
+        .style('text-anchor', 'middle')
+        .style('font-size', '30px')
+        .style('opacity', 0)
+        .merge(texts)
+        .transition()
+        .duration(1000)
+        .style('opacity', 1)
+        .attr('transform', d => `translate(${arc.centroid(d)[0]}, ${arc.centroid(d)[1] - 20})`)
+        .text(d => `${d.data.label}`);
+
+    texts.exit().remove();
+
+    const percentageTexts = svg.selectAll('text.percentage-text')
+        .data(data_ready);
+
+    percentageTexts.enter().append('text')
+        .attr('class', 'percentage-text')
+        .text(d => `${((d.data.value / totalTeams) * 100).toFixed(1)}%`)
+        .attr('transform', d => `translate(${arc.centroid(d)[0]}, ${arc.centroid(d)[1] + 30})`)
+        .style('text-anchor', 'middle')
+        .style('font-size', '40px')
+        .style('opacity', 0)
+        .merge(percentageTexts)
+        .transition()
+        .duration(1000)
+        .style('opacity', 1)
+        .attr('transform', d => `translate(${arc.centroid(d)[0]}, ${arc.centroid(d)[1] + 30})`)
+        .text(d => `${((d.data.value / totalTeams) * 100).toFixed(1)}%`);
+
+    percentageTexts.exit().remove();
+
+    const defs = svg.append('defs');
+    const filter = defs.append('filter')
+        .attr('id', 'drop-shadow')
+        .attr('height', '130%');
+
+    filter.append('feGaussianBlur')
+        .attr('in', 'SourceAlpha')
+        .attr('stdDeviation', 3)
+        .attr('result', 'blur');
+
+    filter.append('feOffset')
+        .attr('in', 'blur')
+        .attr('dx', 3)
+        .attr('dy', 3)
+        .attr('result', 'offsetBlur');
+
+    const feMerge = filter.append('feMerge');
+
+    feMerge.append('feMergeNode')
+        .attr('in', 'offsetBlur');
+    feMerge.append('feMergeNode')
+        .attr('in', 'SourceGraphic');
+
+    svg.append('text')
+        .attr('x', 0)
+        .attr('y', -height / 2 - 30)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '40px')
+        .text('Répartition des victoires et défaites avec MVP');
+}
